29af29e59083bfbf76480650c9b274b50b12ec49
AnandNair4 <dazzanova1@gmail.com>
2026-09-07 23:42:24 +0530

    feat: implement casual attention

--- stage1/models/self_attention.py
+++ stage1/models/self_attention.py
@@ -2,10 +2,15 @@ import numpy as np
 
 np.random.seed(42)
 
-X = np.array([[1.0, 0.0], [0.0, 1.0], [1.0, 1.0]])
+X = np.array([
+    [1.0, 0.0],
+    [0.0, 1.0],
+    [1.0, 1.0]
+])
 
 print("X:")
 print(X)
+
 
 d_model = X.shape[1]
 
@@ -16,6 +21,7 @@ W_V = np.random.randn(d_model, d_model)
 Q = X @ W_Q
 K = X @ W_K
 V = X @ W_V
+
 
 print()
 print("Q:")
@@ -29,15 +35,37 @@ print()
 print("V:")
 print(V)
 
-scores = Q @ K.T
-
-print()
-print("Scores:")
-print(scores)
 
 d_k = K.shape[1]
 
-scores = (Q @ K.T) / np.sqrt(d_k)  # scaled dot-product attention
+scores = (Q @ K.T) / np.sqrt(d_k)
+
+print()
+print("Scores before masking:")
+print(scores)
+
+
+# upper triangular matrix
+mask = np.triu(
+    np.ones((len(X), len(X))),
+    k=1
+)
+
+# replace 1 with -INF
+scores = np.where(
+    mask == 1,
+    -np.inf,
+    scores
+)
+
+# casual attention (tokens can communicate with prev ones , not future)
+print()
+print("Causal mask:")
+print(mask)
+
+print()
+print("Scores after masking:")
+print(scores)
 
 
 def softmax(x):
@@ -51,6 +79,7 @@ attention_weights = softmax(scores)
 print()
 print("Attention weights:")
 print(attention_weights)
+
 
 output = attention_weights @ V
 
